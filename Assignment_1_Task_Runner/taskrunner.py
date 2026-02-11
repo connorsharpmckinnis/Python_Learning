f059712559c693818134d54396d38bb24723895b
connorsharpmckinnis <csharpmckinnis@gmail.com>
2026-02-11 13:37:51 -0500

    progress on assignment 1

--- Assignment_1_Task_Runner/taskrunner.py
+++ Assignment_1_Task_Runner/taskrunner.py
@@ -1,13 +1,20 @@
 import argparse
-from tasks import cipher, json, print
+from tasks import cipher, json, printer
 import yaml
+
+TASKS = {
+    "cipher": cipher,
+    "json": json,
+    "printer": printer
+}
 
 def main():
     print("Hello from assignment-1-task-runner!")
 
-def run_task(task_name:str):
-
-    pass
+def run_task(task_name:str, args):
+    task = TASKS.get(task_name, error)
+    result = task.run()
+    return result
 
 if __name__ == "__main__":
     main()
